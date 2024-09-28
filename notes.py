46046e4379e4514a24fb214eba9ae4b8f6f125d8
Fabio Calcinelli <fabio.calcinelli@gmail.com>
2024-09-28 22:32:07 +0200

    add tests for Piece and Note initialization

--- notes.py
+++ notes.py
@@ -1,14 +1,16 @@
 from datetime import datetime
+from typing import List
+
 
 class Note:
     def __init__(self):
-        self.pieces = []
-        self.timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.last_update_timestamp = self.timestamp
+        self.pieces: List[Piece] = []
+        self.timestamp: str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.last_update_timestamp : str = self.timestamp
 
 class Piece:
     def __init__(self, content: str):
-        self.text = content
-        self.timestamp = datetime.now().strftime
+        self.text : str = content
+        self.timestamp : str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
--- test/test_notes.py
+++ test/test_notes.py
@@ -1,9 +1,48 @@
 import unittest
 
+from unittest.mock import patch
+from datetime import datetime
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+from notes import Piece, Note
+
+
+class TestNotePiece(unittest.TestCase):
+
+    @patch('notes.datetime')
+    def test_piece_initialization(self, mock_datetime):
+        mock_datetime.now.return_value = datetime(2024, 9, 28, 15, 30, 0)
+        mock_datetime.strftime = datetime.strftime
+
+        piece = Piece("This is a test piece.")
+
+        self.assertEqual(piece.text, "This is a test piece.")
+
+        self.assertEqual(piece.timestamp, "2024-09-28 15:30:00")
+
+    @patch('notes.datetime')
+    def test_note_initialization(self, mock_datetime):
+        mock_datetime.now.return_value = datetime(2024, 9, 28, 15, 30, 0)
+        mock_datetime.strftime = datetime.strftime
+
+        note = Note()
+
+        self.assertEqual(note.pieces, [])
+
+        self.assertEqual(note.timestamp, "2024-09-28 15:30:00")
+
+        self.assertEqual(note.last_update_timestamp, "2024-09-28 15:30:00")
+
+    @patch('notes.datetime')
+    def test_add_piece_to_note(self, mock_datetime):
+        mock_datetime.now.return_value = datetime(2024, 9, 28, 15, 30, 0)
+        mock_datetime.strftime = datetime.strftime
+        note = Note()
+        piece = Piece("This is another test piece.")
+        note.pieces.append(piece)
+        self.assertIn(piece, note.pieces)
+        self.assertEqual(len(note.pieces), 1)
+        self.assertEqual(note.pieces[0].text, "This is another test piece.")
+        self.assertEqual(note.pieces[0].timestamp, "2024-09-28 15:30:00")
 
 
 if __name__ == '__main__':
